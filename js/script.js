34dd05e0dc047dbf95cfb573e55fb1358af2fcec
Matteo <matteop18@gmail.com>
2020-01-29 15:04:26 +0100

    3 parte console log dell'oggetto inserito dall'utente

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@
 // Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
 // Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
+// Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età.
+
 var student = {
   'nome': 'Matteo',
   'cognome': 'Petracci',
@@ -11,9 +13,13 @@ var student = {
 
 console.log(student);
 
+// Stampare a schermo attraverso il for in tutte le proprietà.
+
 for (var key in student) {
   console.log(student[key]);
 }
+
+// Creare un array di oggetti di studenti.
 
 var students = [
   {
@@ -33,7 +39,22 @@ var students = [
   }
 ];
 
+// Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+
 for (var i = 0; i < students.length; i++) {
   var listStudents = students[i];
   console.log(listStudents);
+  console.log(students[i].nome);
+  console.log(students[i].cognome);
 }
+
+// Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+var newStudent = {
+    "nome" : prompt("Nome"),
+    "cognome" : prompt("Cognome"),
+    "eta" : prompt("eta")
+  }
+  console.log(newStudent);
+  students.push(newStudent);
+  console.log(students);
